b56224f3155d2b8448232a7d4755bda800132174
Eguo Wang <wangeguo@gmail.com>
2025-06-16 15:47:29 +0800

    feat: add request and response structs for course

--- src/request/course.rs
+++ src/request/course.rs
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-mod course;
-pub mod workflow;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
 
-// Re-exports
-pub use course::*;
+#[derive(Debug, Serialize, Deserialize, ToSchema)]
+pub struct CreateCourseRequest {
+    /// The git repository URL of the course
+    pub repository: String,
+}
--- src/request/mod.rs
+++ src/request/mod.rs
@@ -12,4 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+mod course;
 pub mod workflow;
+
+// Re-exports
+pub use course::*;
+pub use workflow::*;
